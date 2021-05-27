1b285e458072b3559dcbac922e1041e44a9abe26
Shubham Shukla <shubham.shukla@geminisolutions.in>
2021-05-27 17:23:52 +0530

    new changes

--- src/pygem/pygem.py
+++ src/pygem/pygem.py
@@ -1,5 +1,18 @@
-def test():
-   print("hello")
+def test(a, b):
+   """
+   Concatenates two strings
+   Parameters:
+   -----------
+   a: Any string object
+   b: Any string object
+   Returns:
+   String object
+   Examples:
+   ---------
+   >>> test('Hello','World')
+   'Hello World'
+   """
+   return a+' '+b
 
 def test2():
    print("hello2")
