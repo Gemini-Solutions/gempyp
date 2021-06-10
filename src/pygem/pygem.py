41d50848c1c2eecae279681187ec0cb6e8bfc274
Shubham Shukla <shubham.shukla@geminisolutions.in>
2021-06-10 15:53:03 +0530

    added files

--- src/pygem/pygem.py
+++ src/pygem/pygem.py
@@ -21,3 +21,26 @@ def test():
 def test2():
    print("hello3")
    return "hello3"
+
+def parse_argument():
+    pass
+
+class Pygem():
+     def __init__(self):
+         #read the conf here
+         #create suite level result dictionary here
+         #create test level result dictionary here
+         pass
+
+     def execute():
+         #create a pool of process here
+         # create testcase object depending upon type of testcase and call the function run using that object
+         
+         #update the result for each test case
+         #identifying independent task and tasks which have dependency here, create task roster here for executor to run
+         pass
+
+
+if __name__ == "__main__":
+    obj = Pygem()
+    obj.execute()
