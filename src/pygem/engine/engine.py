3b0226512122a846a64b76ea2032029fcdb63724
Shubham Shukla <shubham.shukla@geminisolutions.in>
2021-06-10 15:52:31 +0530

    added files

---
+++ src/pygem/engine/engine.py
@@ -0,0 +1,11 @@
+import libs.Base as Base
+
+class Engine(Base):
+    def __init__(self):
+      #set engine specific config here
+      #call base class __init as well
+      pass
+
+    def run(self):
+       #write logic for running the code here
+       pass
