76c2174ac1c41749a71392144e21a1bf7a5ca4f3
gem-tanvipenumudy <84907761+gem-tanvipenumudy@users.noreply.github.com>
2021-06-29 12:36:52 +0530

    Pushed create.py

---
+++ create.py
@@ -0,0 +1,161 @@
+import json
+import os
+
+def getData(c):
+    print('Enter params for Comparison #'+c)
+    comparison = {}
+    #change params
+    comparison['execute'] = input('execute: ')  
+    comparison['primary_keys'] = input('primary_keys: ').split()  
+    comparison['tolerance'] = input('tolerance: ') 
+    comparison['comp_type'] = 'python'
+    comparison['srcDatabase'] = input('srcDatabase: ') 
+    comparison['srcSchema'] = input('srcSchema: ') 
+    comparison['srcTable'] = input('srcTable: ') 
+    comparison['src_query_path'] = input('src_query_path: ') 
+    comparison['tgtDatabase'] = input('tgtDatabase: ') 
+    comparison['tgtSchema'] = input('tgtSchema: ') 
+    comparison['tgtTable'] = input('tgtTable: ')
+    comparison['tgt_query_path'] = input('tgt_query_path: ')
+    return comparison
+
+def createTest():
+    projectName = input("Enter Project Name: ")
+    filePath = input("Enter Path: ")
+    n = int(input("Number of comparisons to be performed: "))
+    comparisons = {}
+    for i in range(n):
+        comparisons[str(i+1)] = getData(str(i+1))
+    path = os.path.join(filePath, projectName+"_test.json")
+    outFile = open(path, "w")
+    compare = {}
+    compare["comparisons"] = comparisons
+    json.dump(compare, outFile)
+    outFile.close()
+    print(projectName+'_test.json created at '+filePath)
+
+def createSuite():
+    projectName = input("Enter Project Name: ")
+    filePath = input("Enter Path: ")
+    report_email = {}
+    report_email['to'] = input('to: ') 
+    report_email['cc'] = input('cc: ') 
+    report_email['bcc'] = input('bcc: ') 
+    report_email['subject'] = input('subject: ') 
+    report_email['title'] = input('title: ') 
+    path = os.path.join(filePath, projectName+"_suite.json")
+    outFile = open(path, "w")
+    report = {}
+    report["report_email"] = report_email
+    json.dump(report, outFile)
+    outFile.close()
+    print(projectName+'_suite.json created at '+filePath)
+
+def createDB():
+    projectName = input("Enter Project Name: ")
+    filePath = input("Enter Path: ")
+    path = os.path.join(filePath, projectName+"_db.json")
+    outFile = open(path, "w")
+    connection = {}
+    json.dump(connection, outFile)
+    outFile.close()
+    print(projectName+'_db.json created at '+filePath)
+
+def insertConnection():
+    path = input("Enter db.json Path: ")
+    db = open(os.path.join(path), "w")
+    connection = json.load(db)
+    connectionName = input("Enter Connection Name: ")
+    while(connectionName in connection):
+        connectionName = input("Connection already exists. Enter new Connection Name: ")
+    newConnection = {}
+    dbs = ['mysql', 'oracle', 'postgre', 'sqlite']
+    dbType = int(input('Choose:\n1.MySQL\n2.Oracle\n3.PostgreSQL\n4:Sqlite\nResponse: '))
+    newConnection['type'] = dbs[dbType-1] 
+    if(newConnection['type']=='sqlite'):
+        newConnection['location'] = input('location: ') 
+        newConnection['database'] = ''
+        newConnection['user'] = ''
+        #add encryption
+        newConnection['password'] = ''
+        newConnection['host'] = ''
+        newConnection['port'] = ''
+    else:
+        newConnection['location'] = ''
+        newConnection['database'] = input('database: ')
+        newConnection['user'] = input('user: ')
+        #add encryption
+        newConnection['password'] = input('password: ')
+        newConnection['host'] = input('host: ')
+        newConnection['port'] = input('port: ')
+    connection[connectionName]=newConnection
+    json.dump(connection, outFile)
+    outFile.close()
+    print('Inserted Connection '+connectionName)
+
+def ret(str1, dict1, field):
+        if(str1!=''):
+            return str1
+        return dict1[field]
+
+def updateConnection():
+    path = input("Enter db.json Path: ")
+    db = open(os.path.join(path), "w")
+    connection = json.load(db)
+    connectionName = input("Enter Connection Name: ")
+    while(connectionName not in connection):
+        connectionName = input("Connection doesn't exist. Enter valid Connection Name: ")
+    dict1 = connection[connectionName]
+    response = int(input("Change db type?\n1.Yes\n2.No\nResponse: "))
+    if(response==1):
+        dbs = ['mysql', 'oracle', 'postgre', 'sqlite']
+        dbType = int(input('Choose:\n1.MySQL\n2.Oracle\n3.PostgreSQL\n4:Sqlite\nResponse: '))
+        dict1['type'] = dbs[dbType-1]
+    if(dict1['type']=='sqlite'):
+        dict1['location'] = ret(input('location: '), dict1, 'location')
+    else:
+        dict1['database'] = ret(input('database: '), dict1, 'database')
+        dict1['user'] = ret(input('user: '), dict1, 'user')
+        #add encryption
+        dict1['password'] = ret(input('password: '), dict1, 'password')
+        dict1['host'] = ret(input('host: '), dict1, 'host')
+        dict1['port'] = ret(input('port: '), dict1, 'port')
+    connection[connectionName]=dict1
+    json.dump(connection, outFile)
+    outFile.close()
+    print('Updated Connection '+connectionName)
+
+def deleteConnection():
+    path = input("Enter db.json Path: ")
+    db = open(os.path.join(path), "w")
+    connection = json.load(db)
+    connectionName = input("Enter Connection Name: ")
+    while(connectionName not in connection):
+        connectionName = input("Connection doesn't exist. Enter valid Connection Name: ")
+    del connection[connectionName]
+    json.dump(connection, outFile)
+    outFile.close()
+    print('Deleted Connection '+connectionName)
+      
+if __name__ == "__main__":
+    response = int(input("Choose:\n1.Create suite.json\n2.Create test.json\n3.Create db.json\n4.Insert Connection\n5.Update Connection\n6.Delete Connection\n7.Exit\nResponse: "))
+    while(response!=4):
+        if(response==1):
+            createSuite()
+        elif(response==2):
+            createTest()
+        elif(response==3):
+            createDB()
+        elif(response==4):
+            insertConnection()
+        elif(response==5):
+            updateConnection()
+        elif(response==6):
+            deleteConnection()
+        elif(response==7):
+            break
+        else:
+            print('Invalid response!')
+        response = int(input("Choose:\n1.Create suite.json\n2.Create test.json\n3.Create db.json\n4.Insert Connection\n5.Update Connection\n6.Delete Connection\n7.Exit\nResponse: "))
+
+        
