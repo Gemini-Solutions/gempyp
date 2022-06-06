6de2e91f5f8ee395d581be7db89ec7e89884bc82
uipathCommitTesting <sachin.chand@geminisolutions.com>
2022-06-06 19:36:23 +0530

    cli script

--- tests/configTest/engineTest.py
+++ tests/configTest/engineTest.py
@@ -1,13 +1,13 @@
+from distutils.command.config import config
 from gempyp.config.xmlConfig import XmlConfig
 from gempyp.engine.engine import Engine
-
+from runningFile import string
 
 def test():
 
-    config = XmlConfig("C:\\gemecosystem\\pygem\\tests\\configTest\\sampleTest.xml")
-
+    #config = XmlConfig("C:\\Users\\sa.chand\\Desktop\\QA\\gempyp\\tests\\configTest\\sampleTest.xml")
+    config = XmlConfig(string)
  
-    
     Engine(config)
 
 test()
