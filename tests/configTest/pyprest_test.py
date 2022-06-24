b6bba92ed1d31044dbcfd4fb1f338d251e7f8f50
gem-arpitmishra <arpit.mishra@geminisolutions.com>
2022-06-24 13:22:18 +0530

    changes

--- tests/configTest/pyprest_test.py
+++ tests/configTest/pyprest_test.py
@@ -1,10 +1,14 @@
+from re import X
 from gempyp.config.xmlConfig import XmlConfig
 from gempyp.engine.engine import Engine
 
 
 def test():
 
-    config = XmlConfig("C:\\Users\\an.pandey\\gempyp\\tests\\configTest\\sampleTest_pyprest.xml")
+    # config = XmlConfig("/home/vatsarpit/Desktop/gempyp/gempyp/tests/configTest/sampleTest_pyprest.xml")
+    # config = XmlConfig("./tests/configTest/features_suite.xml")
+    config = XmlConfig("/home/vatsarpit/Desktop/gem_api/gem_apis/gem_apis.xml")
+
     
     Engine(config)
 
