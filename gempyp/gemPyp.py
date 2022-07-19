c6753a11c7da47d281d2a5e1bdcf4d88bb1092d9
gem-tanyaagarwal <tanya.agarwal@geminisolutions.com>
2022-07-19 15:32:01 +0530

    running testcases on the basis of category

--- gempyp/gemPyp.py
+++ gempyp/gemPyp.py
@@ -32,6 +32,8 @@ class Gempyp:
         parser.add_argument('-bridge_token','-token',dest='BRIDGE_TOKEN',type=str, required=False)
         parser.add_argument('-username','-username',dest='USERNAME',type=str, required=False)
         parser.add_argument('-output_folder','-of',dest='OUTPUT_FOLDER',type=str, required=False)
+        parser.add_argument('-category','-category',dest='CATEGORY',type=str, required=False)
+        # parser.add_argument('-set','-set',dest='SET',type=str, required=False)
 
         args = parser.parse_args()
         return args
