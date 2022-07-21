8e364325a13e67144d3180ae28057a4fe99e07bf
gem-ankitapandey <84368760+gem-ankitapandey@users.noreply.github.com>
2022-07-21 12:15:33 +0530

    Update dataUpload.py

--- gempyp/engine/dataUpload.py
+++ gempyp/engine/dataUpload.py
@@ -6,24 +6,24 @@ from gempyp.libs import common
 import logging
 
 
-def _getHeaders(bridgeToken):
+def _getHeaders(bridgeToken, user_name):
 
-    return {"Content-Type": "application/json", "bridgetoken": bridgeToken}
+    return {"Content-Type": "application/json", "bridgetoken": bridgeToken, "username": user_name}
 
 
 
-def sendSuiteData(payload, bridgeToken, mode="POST"):
+def sendSuiteData(payload, bridgeToken, user_name, mode="POST"):
     try:
-        response = _sendData(payload, DefaultSettings.urls["suiteExec"], bridgeToken, mode)
+        response = _sendData(payload, DefaultSettings.urls["suiteExec"], bridgeToken, user_name, mode)
         if response.status_code == 201:
             logging.info("data uploaded successfully")
 
     except Exception as e:
         logging.error(traceback.format_exc())
 
-def sendTestcaseData(payload, bridgeToken):
+def sendTestcaseData(payload, bridgeToken, user_name):
     try:
-        response = _sendData(payload, DefaultSettings.urls["testcases"], bridgeToken, method="POST")
+        response = _sendData(payload, DefaultSettings.urls["testcases"], bridgeToken, user_name, method="POST")
 
         if response.status_code == 201:
             logging.info("data uploaded successfully")
@@ -32,13 +32,13 @@ def sendTestcaseData(payload, bridgeToken):
         logging.error(traceback.format_exc())
 
 
-def _sendData(payload, url, bridgeToken, method="POST"):
+def _sendData(payload, url, bridgeToken, user_name, method="POST"):
 
     response = requests.request(
         method=method,
         url=url,
         data=payload,
-        headers=_getHeaders(bridgeToken),
+        headers=_getHeaders(bridgeToken, user_name),
     )
     logging.info(f"status: {response.status_code}")
     response.raise_for_status()
