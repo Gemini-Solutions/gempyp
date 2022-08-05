0b919a88d6d3f47166412ee8e1cd5a380f5ac5a6
gem-tanyaagarwal <tanya.agarwal@geminisolutions.com>
2022-08-05 16:08:35 +0530

    Gempyp-69

--- tests/sample_testcase/runningFile.py
+++ tests/sample_testcase/runningFile.py
@@ -7,7 +7,7 @@ obj = Gempyp()
 
 """Giving the default config file location  and default mail"""
 
-obj.config = "GIT:https://github.com/Gemini-Solutions/gempyp/blob/dev/tests/configTest/Gempyp_Test_suite.xml:dev"
+obj.config = ""
 obj.MAIL = "8979149361t@gmail.com"
 
 
