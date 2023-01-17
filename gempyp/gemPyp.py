3dc4525b72146707c4ebfb8af3380809417abaee
Ankita Pandey <ankita.pandey@geminisolutions.com>
2023-01-17 19:39:53 +0530

    fixing expected testcase count

--- gempyp/gemPyp.py
+++ gempyp/gemPyp.py
@@ -40,7 +40,7 @@ class Gempyp:
         parser.add_argument('-threads','-t',dest='THREADS',type=str, required=False)
         parser.add_argument('-bridge_token','-token',dest='BRIDGE_TOKEN',type=str, required=False)
         parser.add_argument('-username','-username',dest='USERNAME',type=str, required=False)
-        parser.add_argument('output_folder-','-of',dest='OUTPUT_FOLDER',type=str, required=False)
+        parser.add_argument('-output_folder','-of',dest='OUTPUT_FOLDER',type=str, required=False)
         parser.add_argument('-category','-category',dest='CATEGORY',type=str, required=False)
         parser.add_argument('-run_id','-run_id',dest='RUN_ID',type=str, required=False)
         parser.add_argument('-tc','-testcase_list',dest='TESTCASE_LIST',type=str, required=False)
