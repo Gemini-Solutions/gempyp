6379be8e7772ca68eb0e29c3b949b5a69f5336d3
sachin <sachin.chand@geminisolutions.com>
2023-05-09 16:51:57 +0530

    bug 3241 code lambda running issue

--- gempyp/data_uploader.py
+++ gempyp/data_uploader.py
@@ -7,70 +7,80 @@ from cryptography.fernet import Fernet
 from gempyp.config.DefaultSettings import encrypt_key, default_baseurl
 import sys
 import re
-
+import traceback
 def dataUploader(file_path, bridge_token):
-    
-    # Reading the file sent by the user
-    f = open(file_path)
-    encData = f.read()
-    f.close()
-    #decryping the file here
-    fernet = Fernet(encrypt_key)
-    data = fernet.decrypt(encData).decode()
-    data = ast.literal_eval(data)
-    if bridge_token:
-        data['bridge_token'] = bridge_token
-    if not data['base_url']:
-        data['base_url'] = default_baseurl
-    print("Base Url Used:",data['base_url'])
-    response = dataUpload._sendData(" ",data['base_url'], data['bridge_token'], data['user_name'],"GET")
-    print("Calling Base Url",response.status_code)
-    if response.status_code == 200:
-        url_enter_point = response.json()
-        data["urls"] = url_enter_point["data"]    
-        if 'suite_data' in data:
-            #trying to upload suite data 
-            payload = str(data['suite_data'][-1])
-            method = "POST"
-            if len('suite_data') == 1:
-                method = "PUT" 
-            response = dataUpload._sendData(payload, data['urls']['suite-exe-api'],data['bridge_token'], data['user_name'], method)
-            print("Suite data API Response")
-            if response.status_code == 201 or response.status_code == 200:
-                logging.info('Suite Data is Uploaded Successfully')
-                d = ast.literal_eval(data['suite_data'][0])
-                s_run_id = d['s_run_id']
-                print('Jewel Link',f"{data['urls']['jewel-url']}/#/autolytics/execution-report?s_run_id={s_run_id}")
-                del data['suite_data']
-            else:
-                print('Suite data is not Uploaded, therefore terminating the further execution')
+
+    try:
+        # Reading the file sent by the user
+        f = open(file_path)
+        encData = f.read()
+        f.close()
+        #decryping the file here
+        fernet = Fernet(encrypt_key)
+        data = fernet.decrypt(encData).decode()
+        data = ast.literal_eval(data)
+        if bridge_token:
+            data['bridge_token'] = bridge_token
+        if not data['base_url']:
+            data['base_url'] = default_baseurl
+        print("Base Url Used:",data['base_url'])
+        response = dataUpload._sendData(" ",data['base_url'], data['bridge_token'], data['user_name'],"GET")
+        print("Calling Base Url",response.status_code)
+        if response.status_code == 200:
+            url_enter_point = response.json()
+            data["urls"] = url_enter_point["data"]  
+            try:  
+                if 'suite_data' in data:
+                    #trying to upload suite data 
+                    payload = str(data['suite_data'][-1])
+                    method = "POST"
+                    if len('suite_data') == 1:
+                        method = "PUT" 
+                    response = dataUpload._sendData(payload, data['urls']['suite-exe-api'],data['bridge_token'], data['user_name'], method)
+                    print("Suite data API Response")
+                    if response.status_code == 201 or response.status_code == 200:
+                        logging.info('Suite Data is Uploaded Successfully')
+                        d = ast.literal_eval(data['suite_data'][0])
+                        s_run_id = d['s_run_id']
+                        print('Jewel Link',f"{data['urls']['jewel-url']}/#/autolytics/execution-report?s_run_id={s_run_id}")
+                        del data['suite_data']
+                    else:
+                        print('Suite data is not Uploaded, therefore terminating the further execution')
+                        sys.exit()
+            except Exception:
+                traceback.print_exc()
                 sys.exit()
-        if 'testcases' in data:
-            # trying to upload testcase
-            i = -1
-            loop_count = 1
-            testcases_length = len(data['testcases'])
-            while loop_count <= testcases_length:
-                loop_count += 1
-                response = dataUpload._sendData(data['testcases'][i], data['urls']['test-exe-api'],data['bridge_token'], data['user_name'])
-                if response.status_code == 200 or response.status_code == 201:
-                    print("Testcase is uploaded successfully")
-                    del data['testcases'][i]
-                            
-        if 'suite_data' not in data and len(data['testcases']) == 0:
-            #removing the file in case of all the testcase and suite data uploaded successfully
-            print("Removing File")
-            os.remove(file_path)
+            try:       
+                if 'testcases' in data:
+                    # trying to upload testcase
+                    i = -1
+                    loop_count = 1
+                    testcases_length = len(data['testcases'])
+                    while loop_count <= testcases_length:
+                        loop_count += 1
+                        response = dataUpload._sendData(data['testcases'][i], data['urls']['test-exe-api'],data['bridge_token'], data['user_name'])
+                        if response.status_code == 200 or response.status_code == 201:
+                            print("Testcase is uploaded successfully")
+                            del data['testcases'][i]
+            except Exception:
+                traceback.print_exc()
+                                
+            if 'suite_data' not in data and len(data['testcases']) == 0:
+                #removing the file in case of all the testcase and suite data uploaded successfully
+                print("Removing File")
+                os.remove(file_path)
+            else:
+                # Rewriting the data in json file at same location
+                print("Updating File")
+                data = str(data)
+                fernet = Fernet(encrypt_key)
+                data = fernet.encrypt(data.encode())
+                with open(file_path,'wb') as w:
+                    w.write(data)
+                # data = json.loads(data)
+        elif re.search('50[0-9]',str(response.status_code)):
+                logging.info("Their is some problem on server side, Please try again after sometime")
         else:
-            # Rewriting the data in json file at same location
-            print("Updating File")
-            data = str(data)
-            fernet = Fernet(encrypt_key)
-            data = fernet.encrypt(data.encode())
-            with open(file_path,'wb') as w:
-                w.write(data)
-            # data = json.loads(data)
-    elif re.search('50[0-9]',str(response.status_code)):
-            logging.info("Their is some problem on server side, Please try agai after sometime")
-    else:
-            logging.info("Some Error From the Client Side maybe Username or Bridgetoken, Therefore Terminating Execution")
+                logging.info("Some Error From the Client Side maybe Username or Bridgetoken, Therefore Terminating Execution")
+    except Exception:
+        traceback.print_exc()
