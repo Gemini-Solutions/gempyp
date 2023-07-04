e5c93d199982ab0d669f8bba652eda9cb1fb8930
gem-tanyaagarwal <103136246+gem-tanya-agarwal@users.noreply.github.com>
2023-07-04 18:12:42 +0530

    Update PytestTestcases.py

--- PytestTestcases.py
+++ PytestTestcases.py
@@ -22,7 +22,7 @@ def test_loop_assertion():
         if not any(num % div == 0 for div in range(2, num))
     })
 
-@gem_pytest.gpytest.fixture
+@gem_pytest.pytest.fixture
 def setup_data():
     data = [1, 2, 3, 4, 5]
     return data
@@ -33,14 +33,14 @@ def test_fixture_marker(setup_data):
     obj.reporter.addRow("Details for the assertion expression","Expression is sum(setup_data) > 15 where setup_data function is called with the help of fixture marker",obj.status.INFO)
     obj.assert_(sum(setup_data) > 15)
 
-@gem_pytest.gpytest.mark.parametrize("num1, num2, expected_sum", [(2, 3, 5)])
+@gem_pytest.pytest.mark.parametrize("num1, num2, expected_sum", [(2, 3, 5)])
 def test_integer_parametrize_marker(num1, num2, expected_sum):
     obj=gem_pytest()
     obj.reporter.addRow("Details for the testcase","In this testcase parametrize marker with numbers is used. It checks that a certain input leads to an expected output and it can contain multiple set of values as well",obj.status.INFO)
     obj.reporter.addRow("Details for the assertion expression","Expression is num1 + num2 != expected_sum",obj.status.INFO)
     obj.assert_(num1 + num2 != expected_sum)
 
-@gem_pytest.gpytest.mark.parametrize("string, expected_length", [("Hello", 5)])
+@gem_pytest.pytest.mark.parametrize("string, expected_length", [("Hello", 5)])
 def test_string_parametrize_marker(string, expected_length):
     obj=gem_pytest()
     obj.reporter.addRow("Details for the testcase","In this testcase parametrize marker with string is used. It checks that a certain input leads to an expected output and it can contain multiple set of values as well.",obj.status.INFO)
@@ -57,13 +57,11 @@ def test_function_testcase():
     obj.reporter.addRow("Details of testcase","In this testcase value for assertion is used from the method of another class.",obj.status.INFO)
     obj.assert_(TestFunction.balance==101)
 
-@gem_pytest.gpytest.mark.webtest
+@gem_pytest.pytest.mark.webtest
 def test_custom_Marker():
     obj=gem_pytest()
     obj.reporter.addRow("Details of the testcase","Custom marker is used in this testcase. Custom marker is build in pytest.ini file and we can execute the particular marker as well using pytest -m marker command.",obj.status.PASS)
 
 def test_set_runtype_runmode():
     obj=gem_pytest()
-    # os.environ["JEWEL"]="JEWEL_RUN_TYPE"
-    # os.environ["JEWEL_JOB"]="JEWEL_RUN_MODE"
     obj.reporter.addRow("Details of the testcase","Run type and run mode is set manually in this testcase",obj.status.PASS)
